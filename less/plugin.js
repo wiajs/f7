a7a7446e22386235c57e8edebf155720a1b04a6d
sibyl <sibyl@wia.pub>
2020-03-11 01:06:39 -0400

    update f7

--- less/plugin.js
+++ less/plugin.js
@@ -12,12 +12,13 @@ module.exports = {
     functions.add('framework7_encodeURIComponent', svg => encodeURIComponent(svg.value));
     functions.add('framework7_floor', n => new less.tree.Dimension(Math.floor(n.value)));
     functions.add('framework7_calculateN', n => new less.tree.Dimension(100 / parseFloat(n.value)));
-    functions.add('framework7_coreIconsFont', () => {
-      const iconsFontBase64 = ''; //base64Encode('./src/core/icons/font/framework7-core-icons.woff');
+    functions.add('framework7_coreIconsFont', () => {		
+			console.log(process.cwd());
+      const iconsFontBase64 = ''; // base64Encode('../icons/font/framework7-core-icons.woff');
       return iconsFontBase64;
     });
     functions.add('framework7_skeletonFont', () => {
-      const iconsFontBase64 = ''; //base64Encode('./src/core/icons/font/framework7-skeleton.woff');
+      const iconsFontBase64 = ''; // base64Encode('../icons/font/framework7-skeleton.woff');
       return iconsFontBase64;
     });
   },
